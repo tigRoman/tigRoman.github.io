155c86dbd5072bc128e9bf5fa7db0ed3aacacf2a
tigRoman <115407632+tigRoman@users.noreply.github.com>
2023-11-13 17:57:59 +0300

    updTimer

--- js/modules/timer.js
+++ js/modules/timer.js
@@ -1,6 +1,6 @@
 function timer() {
     
-    const deadline = '2023-01-10';
+    const deadline = '2023-11-30';
 
     function getTimeRemaining(endtime) {
         //создаем переменную в которой вычисляем разницу между 
